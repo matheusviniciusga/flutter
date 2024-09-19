13af75bb43e3169dc13443b8b8ec6e9a6789f2f0
Matheus Vinícius <130615198+matheusviniciusga@users.noreply.github.com>
2024-09-19 11:02:36 -0300

    Add files via upload

--- flutter_application_nubank/lib/homePage.dart
+++ flutter_application_nubank/lib/homePage.dart
@@ -11,72 +11,84 @@ class MyStatefulWidget extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Nubank",
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(onPressed: () {}, icon: Icon(Icons.person_outline)),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.visibility)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.question_mark)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.mark_email_read))
-          ],
-        ),
-        body: ListView(
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Text("Conta", style: TextStyle(fontWeight: FontWeight.bold)),
-                        Icon(Icons.arrow_right)
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: Text("RS1000,00"),
-                  ),
-                  Container(
-                    child: Row(
-                      children: [
-                        IconButton(onPressed: () {}, icon: Icon(Icons.pix_outlined)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.money)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.pix_outlined)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.pix_outlined))
-                      ]
-                    ),
-                  ),
-                  Container(
-                    child: Card(
-                      margin: EdgeInsets.symmetric(vertical: 7.0, horizontal: 15.0),
-                      color: Color(0xF5F5F5),
-                      child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Meus Cartões"))
-                    )
-                  ),
-                  Container(
-                    child: Row(
-                      children: [
-                        Card(
-                          margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
-                          color: Color(0xF5F5F5),
-                          child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Novidade. Conheça a ferramn=enta de Controle de Gastos do Nubank"))
-                        ),
-                        Card(
-                          margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
-                          color: Color(0xF5F5F5),
-                          child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Novidade. Conheça a ferramn=enta de Controle de Gastos do Nubank"))
-                        )
-                      ],
-                    ),
-                  )
-                ]
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Conta", style: TextStyle(fontWeight: FontWeight.bold)),
+                    Icon(Icons.arrow_right)
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 15),
+              Container(
+                child: Text("RS1000,00"),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        IconButton.filledTonal(onPressed: () {}, icon: Icon(Icons.pix_outlined), style: ButtonStyle(fixedSize: MaterialStatePropertyAll<Size>(Size.fromRadius(30)))),
+                        Text("Pix")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.money)),
+                        Text("Pagar")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.money)),
+                        Text("Pagar Empréstimo")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.money)),
+                        Text("Transferir")
+                      ],
+                    )
+                  ]
+                ),
+              ),
+              Container(
+                child: Card(
+                  margin: EdgeInsets.symmetric(vertical: 7.0, horizontal: 15.0),
+                  color: Color(0xF5F5F5),
+                  child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Meus Cartões"))
+                )
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+                      color: Color(0xF5F5F5),
+                      child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Novidade. Conheça a ferramn=enta de Controle de Gastos do Nubank"))
+                    ),
+                    Card(
+                      margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+                      color: Color(0xF5F5F5),
+                      child: ListTile(leading: Icon(Icons.card_giftcard), title: Text("Novidade. Conheça a ferramn=enta de Controle de Gastos do Nubank"))
+                    )
+                  ],
+                ),
+              )
+            ]
+          ),
         ),
-      ),
+      ],
     );
   }
 }
--- flutter_application_nubank/lib/main.dart
+++ flutter_application_nubank/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_nubank/homePage.dart';
+import 'package:flutter_application_nubank/appBody.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +15,20 @@ class MyApp extends StatefulWidget {
 class MyStatefulWidget extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Nubank",
-      home: HomePage()
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF8A05BE),
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.person_outline), color: Colors.white, style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFFBA4DE3)))),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.visibility), color: Colors.white70),
+            IconButton(onPressed: () {}, icon: Icon(Icons.question_mark), color: Colors.white70),
+            IconButton(onPressed: () {}, icon: Icon(Icons.mark_email_read), color: Colors.white70)
+          ]
+        ),
+        body: const appBody()
+      )
     );
   }
 }
